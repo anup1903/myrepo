ff2cbf91e7b8196593e7c105b2daaf3c9bc5ecf6
anup1903 <anupd1903@gmail.com>
2024-01-20 01:05:52 +0530

    My new terraform code for sentinal testing

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@
 
 # Provider Block
 provider "aws" {
-  profile = "default" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
+ # profile = "default" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
   region  = "us-east-1"
 }
 
